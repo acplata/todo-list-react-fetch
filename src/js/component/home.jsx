587d08f3e91f2bf736c9a0d790f019bf0594a953
acplata <133425402+acplata@users.noreply.github.com>
2023-07-15 23:31:03 +0000

    intiial

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1 className="mt-5"> To Do List</h1>
+			<div className="container">
+				<div className="card">
+					<ul class="list-group list-group-flush">
+						<li className="list-group-item">An item</li>
+						<li className="list-group-item">A second item</li>
+						<li className="list-group-item">A third item</li>
+						<li className="list-group-item">A fourth item</li>
+						<li className="list-group-item">A fifth item</li>
+					</ul>
+					<div className="card-footer">
+						Items left
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
