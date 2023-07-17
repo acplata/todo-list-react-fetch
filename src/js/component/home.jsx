bd1c498ac1ca6c593666cf6bb7010be5a2c6280e
acplata <133425402+acplata@users.noreply.github.com>
2023-07-17 21:24:42 +0000

    input

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,10 +5,20 @@ const Home = () => {
 	return (
 		<>
 			<h1 className="mt-5"> To Do List</h1>
+
 			<div className="container">
 				<div className="card">
-					<ul class="list-group list-group-flush">
-						<li className="list-group-item">An item</li>
+					<ul className="list-group list-group-flush">
+						<li className="list-group-item">
+							<form>
+								<div className="form-row">
+									<div className="col">
+										<input type="text" class="form-control" placeholder="Task"></input>
+									</div>
+								</div>
+							</form>
+						</li>
+
 						<li className="list-group-item">A second item</li>
 						<li className="list-group-item">A third item</li>
 						<li className="list-group-item">A fourth item</li>
