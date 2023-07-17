eb3ae319f73943e97250d2ddd22dc6e140a1a666
acplata <133425402+acplata@users.noreply.github.com>
2023-07-17 23:13:27 +0000

    version2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //create your first component
+const initialValue = ""
+
 const Home = () => {
+	const [task, setTask] = useState(initialValue);
+	const [taskList, setTaskList] = useState([]);
+
+	const handleChange = (event) => {
+		setTask(event.target.value)
+		console.log(event.target.value)
+	}
+
+	const handleEnter = (event) => {
+		if (event.key === "Enter") {
+			setTaskList([...taskList, task])
+		}
+
+		console.log(event.key)
+	}
+
+	const handleDelete = (deleteTask) => {
+		const filterArray = taskList.filter((element, index) => index !== deleteTask)
+		setTaskList(filterArray);
+	}
+
 	return (
 		<>
 			<h1 className="mt-5"> To Do List</h1>
 
-			<div className="container">
+			<div className="container shadow p-3 mb-5 bg-white rounded" onKeyDown={handleEnter}>
 				<div className="card">
 					<ul className="list-group list-group-flush">
 						<li className="list-group-item">
-							<form>
-								<div className="form-row">
-									<div className="col">
-										<input type="text" class="form-control" placeholder="Task"></input>
-									</div>
+							<div className="form-row">
+								<div className="col">
+									<input type="text" name="task" className="form-control" placeholder="Task" onChange={handleChange}></input>
 								</div>
-							</form>
+							</div>
 						</li>
+						{taskList.map((element, index) => {
+							return (
+								<li className="list-group-item" key={index} >
+									{element}
+									<button type="button" className="btn-close" onClick={() => handleDelete(index)}></button>
+								</li>
+							)
 
-						<li className="list-group-item">A second item</li>
-						<li className="list-group-item">A third item</li>
-						<li className="list-group-item">A fourth item</li>
-						<li className="list-group-item">A fifth item</li>
+						})}
+
 					</ul>
 					<div className="card-footer">
 						Items left
