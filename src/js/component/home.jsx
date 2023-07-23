76dbe7e0da850f6d8d824392892067f3aa805050
Ana Plata <acplata@Anas-MacBook-Air.local>
2023-07-23 17:43:04 -0400

    super update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,48 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const initialValue = ""
 
+const apiUrl = "https://express-blog-xa7v.onrender.com/todo/users/acplata"
+
 const Home = () => {
-	const [task, setTask] = useState(initialValue);
 	const [taskList, setTaskList] = useState([]);
 
+	const [task, setTask] = useState({
+		label: "",
+		done: true,
+	}
+	);
+
+
+	const getTaskList = async () => {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		setTaskList(data);
+	};
+
 	const handleChange = (event) => {
-		setTask(event.target.value)
-		console.log(event.target.value)
+		const updatedTask = { ...task, [event.target.name]: event.target.value };
+		setTask(updatedTask);
 	}
 
-	const handleEnter = (event) => {
-		if (event.key === "Enter") {
-			setTaskList([...taskList, task])
+	const updateAPI = async (newList) => {
+		const response = await fetch(apiUrl, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newList),
+		});
+		if (response.ok) {
+			getTaskList();
 		}
+	};
 
-		console.log(event.key)
-	}
+	const addTaskToAPI = (event) => {
+		if (event.key === "Enter") {
+			const newTaskList = [...taskList, task];
+			updateAPI(newTaskList);
+		}
+	};
 
 	const handleDelete = (deleteTask) => {
-		const filterArray = taskList.filter((element, index) => index !== deleteTask)
-		setTaskList(filterArray);
+		const updateTaskList = taskList.filter((element, index) => index !== deleteTask)
+		updateAPI(updateTaskList);
 	}
 
+	useEffect(() => {
+		getTaskList();
+	}, []);
+
 	return (
-		<>
+		<div onKeyDown={addTaskToAPI}>
 			<h1 className="mt-5"> To Do List</h1>
 
-			<div className="container shadow p-3 mb-5 bg-white rounded" onKeyDown={handleEnter}>
+			<div className="container shadow p-3 mb-5 bg-white rounded">
 				<div className="card">
 					<ul className="list-group list-group-flush">
 						<li className="list-group-item">
 							<div className="form-row">
 								<div className="col">
-									<input type="text" name="task" className="form-control" placeholder="Task" onChange={handleChange}></input>
+									<input type="text" name="label" className="form-control" placeholder="Task" value={task.label} onChange={handleChange}></input>
 								</div>
 							</div>
 						</li>
 						{taskList.map((element, index) => {
 							return (
 								<li className="list-group-item" key={index} >
-									{element}
+									{element.label}
 									<button type="button" className="btn-close" onClick={() => handleDelete(index)}></button>
 								</li>
 							)
@@ -55,7 +83,7 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
